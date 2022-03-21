a7467035b1e45fbfb10b99d9ba9c24e4a5764578
virgiliusnanamanek <84966287+virgiliusnanamanek@users.noreply.github.com>
2022-03-21 13:58:01 +0700

    Membuat module bundler menggunakan webpack

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import 'regenerator-runtime';
+import './styles/style.css';
+import './script/components/nav-bar.js';
+import './script/components/text-header.js';
+import main from './script/views/main.js';
+
+document.addEventListener("DOMContentLoaded", main);
